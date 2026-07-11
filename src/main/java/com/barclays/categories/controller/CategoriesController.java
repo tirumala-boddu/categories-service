82abac6c1da968440cfb7d45beacc9fae223f3bf
tirumala-boddu <tirumala.boddu82@gmail.com>
2026-07-11 17:09:33 +0530

    Feature 1 (#5)

--- src/main/java/com/barclays/categories/controller/CategoriesController.java
+++ src/main/java/com/barclays/categories/controller/CategoriesController.java
@@ -10,7 +10,10 @@ public class CategoriesController {
     //added for PR Testing
     //add for Squash
     //added
-
+    //added
+    //added
+    //Added
+    //Added
     public static void main(String[] args) {
         System.out.println("from mainnnnnnnn");
     }
