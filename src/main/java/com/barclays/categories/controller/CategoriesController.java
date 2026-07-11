662b6ecdc451b33096ac494668e96bc257c17770
tirumala-boddu <tirumala.boddu82@gmail.com>
2026-07-11 19:47:51 +0530

    Qa fix 3

--- src/main/java/com/barclays/categories/controller/CategoriesController.java
+++ src/main/java/com/barclays/categories/controller/CategoriesController.java
@@ -23,6 +23,7 @@ public class CategoriesController {
     //add
     //add
     //add
+    //add
     public static void main(String[] args) {
         System.out.println("from mainnnnnnnn");
     }
